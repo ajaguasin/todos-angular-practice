16c4ffa972ac8bc7e0238ca046197b4921dac997
aj <ajaguasin@gmail.com>
2019-06-03 20:22:07 -0700

    Finish todo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { TodosComponent } from "./components/todos/todos.component";
+const routes: Routes = [{ path: "", component: TodosComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
